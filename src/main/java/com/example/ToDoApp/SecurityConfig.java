172d5433a2d905f7da0ea23cefb66832aec17eed
ohmtob <ohman.tobias@gmail.com>
2019-11-14 12:05:06 +0100

    first try

--- src/main/java/com/example/ToDoApp/SecurityConfig.java
+++ src/main/java/com/example/ToDoApp/SecurityConfig.java
@@ -13,7 +13,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.util.matcher.AndRequestMatcher;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
